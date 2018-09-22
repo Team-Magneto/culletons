68857f026b647a15715e38267002343b41437bad
miltonlopezjr <miltonathm@hotmail.com>
2018-09-22 12:58:23 -0400

    remove .env

--- server/culletons.js
+++ server/culletons.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-require('dotenv').config()
+// require('dotenv').config()
 
 var app = express();
 
